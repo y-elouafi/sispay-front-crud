904f978cf7a356066079e5e353f4a527d95f4f51
y-elouafi <youssef.elouafi.dev@gmail.com>
2020-08-17 12:17:12 +0100

    add test to detail component

--- src/app/modules/user/component/detail/detail.component.spec.ts
+++ src/app/modules/user/component/detail/detail.component.spec.ts
@@ -9,7 +9,7 @@ import { User } from '../../../../models/user';
 import { DetailComponent } from './detail.component';
 
 
-export const mockDog: any = {
+export const mockUser: any = {
   prenom: 'youssef',
   nom: 'elouafi',
   email: 'youssef@gmail.com',
@@ -17,12 +17,12 @@ export const mockDog: any = {
 };
 
 
-describe('DetailComponent', () => {
+fdescribe('DetailComponent', () => {
   let component: DetailComponent;
   let fixture: ComponentFixture<DetailComponent>;
   let httpTestingController: HttpTestingController;
   let api: ApiProvider;
-  let user: User = mockDog;
+  let user: User = mockUser;
 
 
   beforeEach(async(() => {
@@ -43,6 +43,7 @@ describe('DetailComponent', () => {
     fixture = TestBed.createComponent(DetailComponent);
     httpTestingController = TestBed.get(HttpTestingController);
     component = fixture.componentInstance;
+    component.user = user;
     api = fixture.debugElement.injector.get(ApiProvider);
     fixture.detectChanges();
   });
@@ -59,6 +60,21 @@ describe('DetailComponent', () => {
     expect(api).toBeTruthy();
   });
 
+  it('component should get user onSubmit', () => {
+    component.getUserById(1);
+    const req = httpTestingController.expectOne(api.baseUrl+'/users/1');
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('api should getUser data', () => {
+    api.getUser(1).subscribe((res: User) => {
+      expect(res).not.toBe(null);
+      expect(JSON.stringify(res)).toEqual(JSON.stringify(mockUser));
+    });
+    const req = httpTestingController.expectOne(api.baseUrl+'/users/1');
+    req.flush(mockUser);
+    expect(req.request.method).toBe('GET');
+  });
 
 
 });
